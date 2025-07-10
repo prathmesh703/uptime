2629428c46d451168b3efd8648b6fb184e3a71ec
prathmesh703 <pskolpe9890@gmail.com>
2025-07-11 00:31:54 +0530

    middleware updated

--- apps/api/Middleware.ts
+++ apps/api/Middleware.ts
@@ -1,9 +1,25 @@
 import type { NextFunction, Request, Response } from "express";
-
+import jwt from "jsonwebtoken"
+import { JWT_TOKEN } from "./config";
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers['authorization'];
- 
-    req.userId = "8";
+    const authorization = req.headers['authorization'];
+    if(!authorization){
+        return res.status(401).json({
+            error:"user not authenticated"
+        })
+    }
+    const token = authorization.split(' ')[1];
+    if(!token){
+        return res.status(401).json({
+            error:"error in logging"
+        })
+    }
+    const decoded = jwt.verify(token, JWT_TOKEN);
+    if(!decoded || !decoded.sub){
+        return res.status(401).json({error:"unauthorized"});
+    }
+
+    req.userId = decoded.sub as string;
     next()
 }
