af48c6731e7c28fbdb668667bcab7fbcbb7bc433
prathmesh703 <pskolpe9890@gmail.com>
2025-09-14 17:53:40 +0530

    return

--- apps/frontend/components/appbar.tsx
+++ apps/frontend/components/appbar.tsx
@@ -1,4 +1,5 @@
 "use client"
+import  { useRouter } from 'next/navigation';
 
 import {
     ClerkProvider,
@@ -10,12 +11,17 @@ import {
   } from '@clerk/nextjs'
 
   export function Appbar() {
+    const router = useRouter();
    return  <ClerkProvider>
       <div className='flex justify-between items-center p-4 bg-gray-900/95 backdrop-blur-sm border-b border-gray-800 sticky top-0 z-50'>
-        <div className='text-5xl md:text-4xl font-bold text-white align-center leading-tight'>Uptime</div>
-        <div className='flex content-center'>
+        <button onClick={()=>router.push("/")} className='text-5xl cursor-pointer md:text-4xl font-bold text-white align-center leading-tight'>Uptime</button>
+        <div className='flex gap-2 content-center'>
           <SignedOut>
+            <div className='cursor-pointer flex content-center'>
             <SignInButton />
+            </div>
+            
+
             <SignUpButton>
               <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                 Sign Up
